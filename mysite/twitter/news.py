3588dc3dceec6aaf773cd6cbd6de09200b4b274a
Matthew Lee <matthewbeaverlee@gmail.com>
2020-05-11 12:24:52 -0700

    Update news.py

--- mysite/twitter/news.py
+++ mysite/twitter/news.py
@@ -1,6 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
-URL = 'https://fftoday.com/stats/players'
+URL = 'https://fftoday.com/stats/players?Pos=QB'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
-results = soup.find(id='')
+results = soup.find()
+player_data = results.find_all(class_='table')
+for player in player_data:
+    player_name = player.find('span', class_='bodycontent')
+    print(player_name)
