99c89e13a85765c4a1114f664e7204676c771716
Matthew Lee <matthewbeaverlee@gmail.com>
2020-05-15 10:18:36 -0700

    Update news.py

--- mysite/twitter/news.py
+++ mysite/twitter/news.py
@@ -4,7 +4,8 @@ URL = 'https://fftoday.com/stats/players?Pos=QB'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find()
-player_data = results.find_all(class_='table')
+player_data = results.find_all(class_='bodycontent')
+print(player_data)
 for player in player_data:
-    player_name = player.find('span', class_='bodycontent')
+    player_name = player.find('span', class_='')
     print(player_name)
