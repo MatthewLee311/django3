19ea782a0d7f309fc05eb8968ad95b1f2778afc4
Matthew Lee <matthewbeaverlee@gmail.com>
2020-05-22 11:22:08 -0700

    Update fantasy.py

--- mysite/twitter/fantasy.py
+++ mysite/twitter/fantasy.py
@@ -1,12 +1,11 @@
 import requests
+import re
 from bs4 import BeautifulSoup
 URL = 'https://fftoday.com/stats/players?Pos=QB'
 page = requests.get(URL)
 soup = BeautifulSoup(page.content, 'html.parser')
 results = soup.find()
 player_data = results.find_all(class_='bodycontent')
-print(len(player_data), type(player_data), type(player_data[0]), len(player_data[0]))
 for player in player_data:
-    player_url = player.find_all('a')
-    print(player_url)
-    url = player_url.find('href')
+    link = re.search("href",str(soup))
+    print(link)
